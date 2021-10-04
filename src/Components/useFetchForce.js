d06e1df69e8ca598ef2e395a121b6c8a80cd79f2
Jonathan PD <jonathan@produzdigital.com.br>
2021-10-04 19:17:36 -0300

    Request Arranging Function

--- src/Components/useFetchForce.js
+++ src/Components/useFetchForce.js
@@ -9,13 +9,16 @@ const useFetchForce = () => {
         setError(false);
         const force1 = fetch('https://swapi.dev/api/people/1');
         const force2 = fetch('https://swapi.dev/api/people/4');
-        Promise.race([force1, force2]).then(async (winning_force) => { //console.log('succeeded', winning_force);        
-        const json = await winning_force.json();
-        setForce(json);
-        setLoading(false);
-        }).catch((reason) => {
-        console.log('failed', reason);
-        setError(`ERRO! Sem resposta de ambas as forças. Verifique o console log para mais detalhes.`);        
+        Promise.race([force1, force2])
+        .then(async (winning_force) => {
+            //console.log('succeeded', winning_force);        
+            const json = await winning_force.json();
+            setForce(json);
+            setLoading(false);
+        })
+        .catch((reason) => {
+            console.log('failed', reason);
+            setError(`ERRO! Sem resposta de ambas as forças. Verifique o console log para mais detalhes.`);        
         });
     }
     return { force, error, loading, request };
